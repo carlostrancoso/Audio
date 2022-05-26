a5be6a6b57d60d3332df4177ab5f6d1595eb48bf
Carlos Trancoso <90407115+carlostrancoso@users.noreply.github.com>
2022-05-26 23:33:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 let busto;
-let map;
 var mic;
-var easycam;
-var myCanvas;
 
 function preload() {
   busto = loadModel('BUSTO_15_GU.obj', true);
-  map = loadImage('Busto 15_LO.jpg');
 }
 
 function setup() {
@@ -18,27 +14,22 @@ function setup() {
   //var y = (windowHeight - height) / 2;
   //cnv.position(x, y);
 
-  easycam = createEasyCam();
-
   mic = new p5.AudioIn();
   mic.start();
 
-  document.oncontextmenu = function() { return false; }
+  
   
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  easycam.setViewport([0,0,windowWidth, windowHeight]);
 }
 
 function draw() {
   //background(0);
 
-  getAudioContext().resume();
-
   var vol = mic.getLevel();
-  console.log(vol);
+  //console.log(vol);
 
   
   scale(1.5+vol*45);
@@ -50,8 +41,7 @@ function draw() {
   pointLight(0, 0, 250, -100, -100, height/2);
   pointLight(0, 250, 0, 100, 150, height/2);
   ambientLight(200);
-  //normalMaterial();
-  texture(map);
+  normalMaterial();
   //(5,5,0.2);
   //rotateX(PI/6);
   //rotateY(-1);
@@ -60,7 +50,10 @@ function draw() {
   rotateZ(PI);
   rotateY(HALF_PI);
   model(busto);
-  
-  
-  
+  orbitControl(-2,-2,0);
+
+}
+
+function touchStarted() {
+  getAudioContext().resume();
 }
