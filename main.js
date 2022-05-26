eed14c4cf60d1532a45d1c4713bc0f999c876f42
Carlos Trancoso <90407115+carlostrancoso@users.noreply.github.com>
2022-05-26 23:49:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,12 @@ let busto;
 var mic;
 
 function preload() {
-  busto = loadModel('BUSTO_15_GU.obj', true);
+  busto = loadModel('BUSTO_12.obj', true);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  //var cnv = createCanvas(300, 450, WEBGL);
-  //debugMode();
   background(0);
-  //var x = (windowWidth - width) / 2;
-  //var y = (windowHeight - height) / 2;
-  //cnv.position(x, y);
 
   mic = new p5.AudioIn();
   mic.start();
@@ -31,9 +26,7 @@ function draw() {
   var vol = mic.getLevel();
   //console.log(vol);
 
-  
   scale(1.5+vol*45);
-
  
   let locX = mouseX - width / 2;
   let locY = mouseY - height / 2;
@@ -42,9 +35,6 @@ function draw() {
   pointLight(0, 250, 0, 100, 150, height/2);
   ambientLight(200);
   normalMaterial();
-  //(5,5,0.2);
-  //rotateX(PI/6);
-  //rotateY(-1);
   
   noStroke();
   rotateZ(PI);
